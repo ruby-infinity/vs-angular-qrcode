76c5034169cd2ac56f2ad59b5ee99a171ed55e32
vs-zhang <vincentzhang1209@gmail.com>
2015-04-08 12:20:34 -0400

    * add more color and background setting * refactor code more readable

--- src/angular-qrcode.js
+++ src/angular-qrcode.js
@@ -6,48 +6,67 @@
 
 angular.module('vs.qrcode', []).
   directive('qrcode', [function () {
+    var drawQrcode = function (element, attrs) {
+      console.log(attrs);
+      //params setup
+      var data = attrs.data,
+        size = attrs.size || 300,
+        img = attrs.img,
+        typeNumber = attrs.typeNumber || 4,
+        color = attrs.color || '#000',
+        background = attrs.background || '#fff',
+        imgSize = size / 5,
+        imgPadding = 10,
+        canvas,
+        context,
+        modulesCount,
+        tileSize,
+        qrcode;
+
+      // setup canvas
+      canvas = document.createElement('canvas');
+      context = canvas.getContext('2d');
+      canvas.width = size;
+      canvas.height = size;
+
+      // qrcode data init
+      qrcode = new QRCode(typeNumber, QRErrorCorrectLevel.H);
+      qrcode.addData(data);
+      qrcode.make();
+
+      // draw qr code
+      modulesCount = qrcode.getModuleCount();
+      tileSize = size / modulesCount;
+
+      for (var row = 0; row < modulesCount; row++) {
+        for (var col = 0; col < modulesCount; col++) {
+          var w = (Math.ceil((col + 1) * tileSize) - Math.floor(col * tileSize)),
+            h = (Math.ceil((row + 1) * tileSize) - Math.floor(row * tileSize));
+
+          context.fillStyle = qrcode.isDark(row, col) ? color : background;
+          context.fillRect(Math.round(col * tileSize), Math.round(row * tileSize), w, h);
+        }
+      }
+
+      // draw img
+      if (img) {
+        var image = new Image();
+        image.src = img;
+        context.fillStyle = '#fff';
+        context.fillRect((size - imgSize - imgPadding) / 2, (size - imgSize - imgPadding) / 2, imgSize + imgPadding, imgSize + imgPadding);
+        context.drawImage(image, (size - imgSize) / 2, (size - imgSize) / 2, imgSize, imgSize);
+      }
+
+      // append canvas to element
+      element.append(canvas);
+    };
+
     return {
       restrict: 'E',
       replace: true,
       template: '<div class="qrcode"></div>',
       link: function (scope, element, attrs) {
-        var data = attrs.data,
-            img = attrs.img,
-            size = attrs.size;
-
-        var canvas = document.createElement('canvas');
-        canvas.width = size;
-        canvas.height = size;
-        var context = canvas.getContext('2d');
-
-        element.append(canvas);
-
-        qrcode = new QRCode(4, QRErrorCorrectLevel.M);
-        qrcode.addData(data);
-        qrcode.make();
-
-        var modules = qrcode.getModuleCount();
-
-        var tile = attrs.size/modules;
-
-        var image = new Image();
-        image.src = img;
-
-        var imgSize = size/4;
-        var imgPos = size - imgSize;
-        for (var row = 0; row < modules; row++) {
-          for (var col = 0; col < modules; col++) {
-            var w = (Math.ceil((col + 1) * tile) - Math.floor(col * tile)),
-              h = (Math.ceil((row + 1) * tile) - Math.floor(row * tile));
-
-            context.fillStyle = qrcode.isDark(row, col) ? '#000' : '#fff';
-            context.fillRect(Math.round(col * tile), Math.round(row * tile), w, h);
-          }
-        }
-
-        context.fillStyle = '#fff';
-        context.fillRect(imgPos/2,imgPos/2,imgSize,imgSize);
-        context.drawImage(image, imgPos/2, imgPos/2, imgSize,imgSize);
+        drawQrcode(element, attrs);
       }
     }
   }]);
